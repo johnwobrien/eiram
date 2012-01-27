2d3715404e7b028f5feeb69c03eb08354b464253
Brian To <bxt5647@rit.edu>
2012-01-26 19:15:09 -0500

    Made project.

---
+++ src/main/java/edu/rit/se/sse/eiram/Main.java
@@ -0,0 +1,12 @@
+package edu.rit.se.sse.eiram;
+
+/**
+ * Hello world!
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ src/test/java/edu/rit/se/sse/eiram/AppTest.java
@@ -0,0 +1,21 @@
+package edu.rit.se.sse.eiram;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.junit.Assert.*;
+
+/**
+ * Dummy unit test.
+ */
+public class AppTest {
+
+  /**
+   * Fake test.
+   */
+  @Test
+  public void testApp() {
+    assertTrue(true);
+  }
+}
