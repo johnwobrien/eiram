f9137307385f67179386a013a2345dff8c41abe8
Brian To <bxt5647@rit.edu>
2012-03-02 11:39:03 -0500

    Wrote initial draft of eiram's grammar.

--- src/main/java/edu/rit/se/sse/eiram/Main.java
+++ src/main/java/edu/rit/se/sse/eiram/Main.java
@@ -1,12 +1,14 @@
 package edu.rit.se.sse.eiram;
 
+import edu.rit.se.sse.eiram.grammar.*;
+
 /**
  * Hello world!
  */
 public class Main {
 
   public static void main(String[] args) {
-
+    // TODO
   }
 
 }
